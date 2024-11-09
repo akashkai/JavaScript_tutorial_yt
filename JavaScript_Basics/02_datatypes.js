16bc8e88b12d353e4e09e52f828fd2fef94a8980
akash_kaithele <144932473+akashkai@users.noreply.github.com>
2024-11-09 16:30:05 +0000

    add 02_datatypes file

---
+++ JavaScript_Basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"    //treat entire js code as newer version
+
+//alert(2+2)    //give error because we are using node instead browser
+
+
+let userName="Ram"          //string
+let userAge=18             //number
+let userPresent=true     //boolean 
+let userLocation        //undefined datatype
+let cityTemp=null      //object datatype (it means not set/get due to error/issue any value bot defined)
+
+
+//bigint is also one type of datatype that is used for very big number
+
+console.table([typeof userName,typeof userAge, typeof userPresent, typeof userLocation, typeof cityTemp])
